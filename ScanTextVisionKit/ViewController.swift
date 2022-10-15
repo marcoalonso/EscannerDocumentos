2fde98f2aa013bd74f2e6630ff5aca60ca3de39c
Marco Alonso AP <mrodriguez@alquimiapay.com>
2022-10-15 00:28:50 -0500

    bugs fixed, new features added

--- ScanTextVisionKit/ViewController.swift
+++ ScanTextVisionKit/ViewController.swift
@@ -19,11 +19,22 @@ class ViewController: UIViewController {
     
     var huboCoincidencias: Bool = false
     var palabrasEncontradas: [String] = []
+    let scanVC = VNDocumentCameraViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        scanVC.delegate = self
+        
+        previewDocImage.isUserInteractionEnabled = true
+        previewDocImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(selectDoc)))
     }
+    
+    @objc func selectDoc() {
+        performSegue(withIdentifier: "verDocumento", sender: self)
+    }
+    
+    
     
     private func processImage(_ image: UIImage) {
         guard let cgImage = image.cgImage else { return }
@@ -36,7 +47,7 @@ class ViewController: UIViewController {
         }
     }
     
-    private func configureOCR() {
+    func configureOCR() {
         ocrRequest = VNRecognizeTextRequest { (request, error) in
             guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
             
@@ -56,8 +67,6 @@ class ViewController: UIViewController {
                 self.ocrRequest.recognitionLanguages = ["es-Es"]
                 self.ocrRequest.usesLanguageCorrection = true
             }
-            
-            
         }
         
         
@@ -67,11 +76,12 @@ class ViewController: UIViewController {
                 self.performSegue(withIdentifier: "verTexto", sender: self)
             }
             
-            let accionCancelar = UIAlertAction(title: "Buscar de nuevo", style: .cancel, handler: nil)
-            
+            let accionCancelar = UIAlertAction(title: "Buscar de nuevo", style: .destructive) { _ in
+                self.previewTextInDocument.text = ""
+                self.present(self.scanVC, animated: true)
+            }
             alerta.addAction(accionAceptar)
             alerta.addAction(accionCancelar)
-            
             self.present(alerta, animated: true)
         }
     }
@@ -81,17 +91,24 @@ class ViewController: UIViewController {
             let detalleTexto = segue.destination as! VerTextoViewController
             detalleTexto.palabrasEncontradas = self.palabrasEncontradas
         }
+        
+        if segue.identifier == "verDocumento" {
+            if huboCoincidencias {
+                let detalleDocumento = segue.destination as! VerDocumentoViewController
+                detalleDocumento.recibirDocumentoMostrar = previewDocImage.image
+                detalleDocumento.fotoTomada = true
+            }
+            
+        }
     }
     
     @IBAction func buscarTextoButton(_ sender: UIButton) {
+        processImage(previewDocImage.image ?? UIImage(named: "instrucciones")!)
         configureOCR()
     }
     
     @IBAction func scanButton(_ sender: UIButton) {
-        let scanVC = VNDocumentCameraViewController()
-        scanVC.delegate = self
         present(scanVC, animated: true)
-        
     }
     
 }
@@ -101,17 +118,18 @@ extension ViewController: VNDocumentCameraViewControllerDelegate {
         
         //Si hay por lo menos un documento escaneado:
         if scan.pageCount > 0 {
+            print("DEBUG: scan.pageCount ",scan.pageCount)
             
             //Indica que # de pagina vamos a analizar
             previewDocImage.image = scan.imageOfPage(at: 0)
             
-            processImage(previewDocImage.image!)
-            //processImage(scan.imageOfPage(at: 1))
+//            processImage(scan.imageOfPage(at: 0))
+//            processImage(previewDocImage.image ?? UIImage(named: "instrucciones")!)
             
             controller.dismiss(animated: true)
             
         }
-        configureOCR()
+//        configureOCR()
     }
     
     
